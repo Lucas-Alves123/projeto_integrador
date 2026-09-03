909c0f3c5ba25c744731e0e169169073376099f5
Myllena Navarro <myllena.cristiane250@gmail.com>
2026-09-03 08:16:48 -0300

    Ajuste simples na aplicação

--- MesaDJ/src/StatusDisplayThread.java
+++ MesaDJ/src/StatusDisplayThread.java
@@ -14,7 +14,7 @@ public class StatusDisplayThread implements Runnable {
     public void run() {
         while (running) {
             try {
-                Thread.sleep(20000); // Atualiza a cada 20 segundos
+                Thread.sleep(2000);
                 
                 System.out.println("\n\n=== [ PAINEL AO VIVO DA MESA DJ ] ===");
                 for (Map.Entry<String, InstrumentAudioThread> entry : instruments.entrySet()) {
@@ -22,7 +22,7 @@ public class StatusDisplayThread implements Runnable {
                     System.out.println(" > " + entry.getKey().toUpperCase() + " : " + status);
                 }
                 System.out.println("=======================================");
-                System.out.println("Comandos: play <inst>, pause <inst>, add <inst> <arq>, bpm <inst> <valor>, volume <inst> <0-100>, exit");
+                System.out.println("Comandos: play <inst>, pause <inst>, add <inst> <arq>, bpm <inst> <valor>, volume <inst> <0-100>, exit/sair");
                 System.out.print("DJ> ");
                 
             } catch (InterruptedException e) {
